51680fcc681e47d671ef8c9dd30617de39ffe901
David Tauraso <david318@me.com>
2020-04-17 10:45:51 -0700

    Tried to optimize but reverted back

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -103,6 +103,7 @@ class SortingRobot:
             # put item back
             self.move_left()
             self.swap_item()
+            
         else:
             # case 2: held item is greater than the (i + 1)th item
             self.swap_item()
@@ -130,7 +131,8 @@ class SortingRobot:
             # try to swap while we haven't reached the end of the list
             while(self.can_move_right()):
                 self.compare_items_and_swap_in_list()
-
+                # if the light is on
+                # if the light is off
             # send robot's position back to begining of list
             while(self.can_move_left()):
                 self.move_left()
